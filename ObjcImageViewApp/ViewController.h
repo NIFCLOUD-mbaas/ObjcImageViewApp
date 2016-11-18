0d2b93df0d06c721eaa10fa9f726dc07338dd0c3
oonoyosp <oonoyosp@pck-133-330.local>
2016-11-18 15:01:51 +0900

    first setup

---
+++ ObjcImageViewApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjcImageViewApp
+//
+//  Created by oono on 2016/11/18.
+//  Copyright © 2016年 Nifty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios,'8.0'
+
+target "ObjcImageViewApp" do
+   pod 'NCMB', :git => 'https://github.com/NIFTYCloud-mbaas/ncmb_ios.git'
+end
+
